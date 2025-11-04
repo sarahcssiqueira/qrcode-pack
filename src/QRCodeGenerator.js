7e264705252c6bfff2c91bbdef097fa74905a7ee
sarahcssiqueira <sarahcso@hotmail.com>
2025-11-04 20:51:13 -0300

    feat: adds support for bigger strings

--- src/QRCodeGenerator.js
+++ src/QRCodeGenerator.js
@@ -6,11 +6,23 @@ export default class QRCodeGenerator {
     this.modules = null;
     this.reserved = null;
     // corrected tables: L/M/Q/H are data codewords (totalCodewords - ecCodewordsPerBlock)
+    // Extended versionTable up to version 15 for even longer data support
     this.versionTable = {
-      1: { L: 19, M: 16, Q: 13, H: 9, align: [], totalCodewords: 26, ecCodewordsPerBlock: {L:7, M:10, Q:13, H:17}, blocks: {L:1,M:1,Q:1,H:1} },
-      2: { L: 34, M: 28, Q: 22, H: 16, align: [6, 18], totalCodewords: 44, ecCodewordsPerBlock: {L:10,M:16,Q:22,H:28}, blocks: {L:1,M:1,Q:1,H:1} },
-      3: { L: 55, M: 44, Q: 34, H: 26, align: [6, 22], totalCodewords: 70, ecCodewordsPerBlock: {L:15,M:26,Q:36,H:44}, blocks: {L:1,M:1,Q:2,H:2} },
-      4: { L: 80, M: 64, Q: 48, H: 36, align: [6, 26], totalCodewords: 100, ecCodewordsPerBlock: {L:20,M:36,Q:52,H:64}, blocks: {L:1,M:2,Q:2,H:4} },
+      1:  { L: 19,  M: 16,  Q: 13,  H: 9,  align: [],        totalCodewords: 26,  ecCodewordsPerBlock: {L:7,  M:10, Q:13, H:17}, blocks: {L:1, M:1, Q:1, H:1} },
+      2:  { L: 34,  M: 28,  Q: 22,  H: 16, align: [6, 18],   totalCodewords: 44,  ecCodewordsPerBlock: {L:10, M:16, Q:22, H:28}, blocks: {L:1, M:1, Q:1, H:1} },
+      3:  { L: 55,  M: 44,  Q: 34,  H: 26, align: [6, 22],   totalCodewords: 70,  ecCodewordsPerBlock: {L:15, M:26, Q:36, H:44}, blocks: {L:1, M:1, Q:2, H:2} },
+      4:  { L: 80,  M: 64,  Q: 48,  H: 36, align: [6, 26],   totalCodewords: 100, ecCodewordsPerBlock: {L:20, M:36, Q:52, H:64}, blocks: {L:1, M:2, Q:2, H:4} },
+      5:  { L: 108, M: 86,  Q: 62,  H: 46, align: [6, 30],   totalCodewords: 134, ecCodewordsPerBlock: {L:26, M:48, Q:72, H:88}, blocks: {L:1, M:2, Q:2, H:2} },
+      6:  { L: 136, M: 108, Q: 76,  H: 60, align: [6, 34],   totalCodewords: 172, ecCodewordsPerBlock: {L:36, M:64, Q:96, H:112}, blocks: {L:2, M:4, Q:4, H:4} },
+      7:  { L: 156, M: 124, Q: 88,  H: 66, align: [6, 22, 38], totalCodewords: 196, ecCodewordsPerBlock: {L:40, M:72, Q:108, H:130}, blocks: {L:2, M:4, Q:6, H:6} },
+      8:  { L: 194, M: 154, Q: 110, H: 86, align: [6, 24, 42], totalCodewords: 242, ecCodewordsPerBlock: {L:48, M:88, Q:132, H:156}, blocks: {L:2, M:4, Q:6, H:6} },
+      9:  { L: 232, M: 182, Q: 132, H: 100, align: [6, 26, 46], totalCodewords: 292, ecCodewordsPerBlock: {L:60, M:110, Q:160, H:192}, blocks: {L:2, M:5, Q:8, H:8} },
+      10: { L: 274, M: 216, Q: 154, H: 122, align: [6, 28, 50], totalCodewords: 346, ecCodewordsPerBlock: {L:72, M:130, Q:192, H:224}, blocks: {L:4, M:5, Q:8, H:8} },
+      11: { L: 324, M: 254, Q: 180, H: 140, align: [6, 30, 54], totalCodewords: 404, ecCodewordsPerBlock: {L:80, M:150, Q:224, H:264}, blocks: {L:4, M:5, Q:10, H:10} },
+      12: { L: 370, M: 290, Q: 206, H: 158, align: [6, 32, 58], totalCodewords: 466, ecCodewordsPerBlock: {L:96, M:176, Q:260, H:308}, blocks: {L:4, M:8, Q:12, H:12} },
+      13: { L: 428, M: 334, Q: 244, H: 180, align: [6, 34, 62], totalCodewords: 532, ecCodewordsPerBlock: {L:104, M:198, Q:288, H:352}, blocks: {L:4, M:9, Q:16, H:16} },
+      14: { L: 461, M: 365, Q: 261, H: 197, align: [6, 26, 46, 66], totalCodewords: 581, ecCodewordsPerBlock: {L:120, M:216, Q:320, H:384}, blocks: {L:4, M:9, Q:16, H:16} },
+      15: { L: 523, M: 415, Q: 295, H: 223, align: [6, 26, 48, 70], totalCodewords: 655, ecCodewordsPerBlock: {L:132, M:240, Q:360, H:432}, blocks: {L:6, M:10, Q:18, H:18} },
     };
     this.initGF();
     this.dataBytes = this.toUtf8Bytes(this.text);
@@ -85,7 +97,17 @@ export default class QRCodeGenerator {
   // Use the capacity (bytes) precomputed in versionTable for byte mode
   getDataCapacityBits(){ const verInfo = this.versionTable[this.version]; const dataBytes = verInfo[this.ecLevel]; return dataBytes * 8; }
   getDataCapacityBytes(){ const verInfo = this.versionTable[this.version]; return verInfo[this.ecLevel]; }
-  chooseVersion(){ for(let v=1; v<=4; v++){ const cap = this.versionTable[v][this.ecLevel]; if(this.dataBytes.length <= cap){ this.version = v; return; } } throw new Error('Data too long for versions 1..4'); }
+  chooseVersion(){
+  // Now supports up to version 15
+  for(let v=1; v<=15; v++){
+      const cap = this.versionTable[v][this.ecLevel];
+      if(this.dataBytes.length <= cap){
+        this.version = v;
+        return;
+      }
+    }
+    throw new Error('Data too long for versions 1..10');
+  }
 
   // ECC
   makeECC(dataBytes){
